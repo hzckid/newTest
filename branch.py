d0c1b93bb7c4f06755217c433a8f30840bec9148
hzckid <hzckid@gmail.com>
2018-11-17 16:28:18 +0800

    add branch.py

---
+++ branch.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+#************************************************
+# *Author*        :huangzhicai
+# *Created Time*  : 2018-11-17 16:27:23
+#**Problem**:
+#**Analyse**:
+#**Get**:
+#**Code**:
+#************************************************
+
+import numpy as np
+import tensorflow as tf
+
+
+def main():
+    pass
+if __name__=="__main__":
+    main()
