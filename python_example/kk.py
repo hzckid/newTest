5f52ed2f022888fef4caa26eaed072a730454f20
hzckid <hzckid@gmail.com>
2018-11-18 11:28:19 +0800

    add python_example with kk.py

---
+++ python_example/kk.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+#************************************************
+# *Author*        :huangzhicai
+# *Created Time*  : 2018年11月18日 星期日 11时27分46秒
+#**Problem**:
+#**Analyse**:
+#**Get**:
+#**Code**:
+#************************************************
+
+import numpy as np
+import tensorflow as tf
+
+
+
+def main():
+    pass
+if __name__=="__main__":
+    main()
